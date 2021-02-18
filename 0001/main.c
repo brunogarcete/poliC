750799a97d09b64b81bbd7e8411f29924c2af80e
Brunei <mediosdeparaguay@gmail.com>
2021-02-18 13:50:17 -0300

    practicas de algoritmica 2 en C

--- 0001/main.c
+++ 0001/main.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"estructuras.c"
-#include"funciones.c"
+#include<math.h>
 
 
-int main(void){
-    persona thais;
-    leerPersona(&thais);
-    system("clear");
-    pPersona(&thais);
+int main(int argc,char**argv) {
+    void proceso(int,int);
+    int a=atoi(argv[1]);
+    int b=atoi(argv[2]);
+    proceso(a,b);
     return 0;
+}
+
+void proceso(int x,int y) {
+    if(fmod((double)x,(double)y) == 0){
+        printf("Es divisor\n");
+    }else{
+        printf("No es divisor\n");
+    }
 }
