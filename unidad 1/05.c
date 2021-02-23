677d36d81440742798fad13494fae1ffc7f08ef7
Brunei <mediosdeparaguay@gmail.com>
2021-02-23 14:09:31 -0300

    v

---
+++ unidad 1/05.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define tam 10
+
+void generarNumeros(int,int,int,int);
+
+int main(void){
+	// numero = rand () % (N-M+1) + M;   // Este está entre M y N
+	// la funcion generar numeros recibe como parametro desde donde leer hasta donde terminar
+	generarNumeros(0,tam,0,0);
+	return 0;
+}
+
+
+void generarNumeros(int i,int j,int mult3,int mult5) {
+
+	// Declaraciones locales
+	int array[tam];
+
+	//trabajo
+	scanf("%d",&array[i]);
+
+	if(array[i]%3 == 0){
+		mult3++;
+	}
+
+	if(array[i]%5 == 0){
+		mult5++;
+	}
+	// condicion
+	if(i < tam){
+		// llamada recursiva
+		generarNumeros((i+1),tam,mult3,mult5);
+	}else{
+		printf("multiplos de 3 = %d multiplos de 5 = %d",mult3,mult5);
+	}
+}
