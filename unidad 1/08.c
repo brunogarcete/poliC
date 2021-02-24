1ebb4502badd869a202830c67d7d4f421a782ce5
Brunei <mediosdeparaguay@gmail.com>
2021-02-23 22:46:17 -0300

    c

---
+++ unidad 1/08.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void proceso (void);
+
+int main(void) {
+    proceso();
+    return 0;
+}
+
+void proceso () {
+    int mult3 = 0;
+    for(int i = 1; i <= 100 ; i++) {
+        if(i%3==0) {
+            mult3++;
+        }
+    }
+    printf("Los numeros multiplos de 3 = %d\n",mult3);
+}
