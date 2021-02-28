d8c23fea763401024bdca01d63bc9c2bfaf01024
Brunei <mediosdeparaguay@gmail.com>
2021-02-28 10:29:37 -0300

    c

---
+++ 0001/malapraxis.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+namespace o1 {
+    int edad = 24;
+}
+
+int main(void) {
+    std::cout << "hola este objeto tiene edad = " << 01::edad << std::endl
+      
+}
